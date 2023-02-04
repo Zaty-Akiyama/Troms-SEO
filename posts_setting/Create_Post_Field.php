35b7ee2ac21d395e48e2e4a94b24ee634aea7541
yuhya <akiyama@zaty.jp>
2023-02-04 17:19:02 +0900

    カスタム投稿とpageに抜粋を追加

--- posts_setting/Create_Post_Field.php
+++ posts_setting/Create_Post_Field.php
@@ -1,7 +1,10 @@
 <?php
 
+if ( !class_exists( 'Create_Post_Field' ) ):
 class Create_Post_Field {
   public function __construct () {
+
+    add_action('wp_loaded', array($this, 'add_excerpt'));
 
   }
 
@@ -18,4 +21,24 @@ class Create_Post_Field {
    * 
    * 隠しmetakey は _meta
    */
+
+   public function add_excerpt () {
+    $public_args = array(
+      'public' => true,
+      '_builtin' => false
+    );
+
+    $custom_post_types = get_post_types( $public_args, 'names', 'and' );
+
+    $default_post = array('post', 'page');
+    $post_types = $default_post + $custom_post_types;
+
+    foreach ($post_types as $post_type) {
+      add_post_type_support( $post_type, 'excerpt' );
+    }
+   }
 }
+
+new Create_Post_Field;
+
+endif;
